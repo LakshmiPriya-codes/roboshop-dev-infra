0e09eac5b2bd1cb62ab7b3e81ed8798b376db4aa
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-07-30 19:15:19 +0530

    api doc added

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group_rule" "mongodb_bastion" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id =  local.user_sg_id
+  source_security_group_id =  local.bastion_sg_id
   security_group_id = local.mongodb_sg_id
 }
 
