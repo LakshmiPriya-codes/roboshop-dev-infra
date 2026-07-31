dbee8caf6ad7a4bcdcf19c708b176ec760f2a2ac
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-07-31 18:45:24 +0530

    api doc added

--- 40-database/r53.tf
+++ 40-database/r53.tf
@@ -17,10 +17,10 @@ resource "aws_route53_record" "redis" {
 }
 
 
-resource "aws_route53_record" "rabbitmq" {
+/* resource "aws_route53_record" "rabbitmq" {
   zone_id = var.zone_id
   name    = "rabbitmq-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 1
   records = [aws_instance.rabbitmq.private_ip]
-}
+} */
