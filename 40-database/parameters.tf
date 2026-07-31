239816b422dc41a3abfaf88e097b0e00452aadf7
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-07-31 19:56:07 +0530

    api doc added

--- 40-database/parameters.tf
+++ 40-database/parameters.tf
@@ -1,5 +1,4 @@
-resource "aws_ssm_parameter" "sg_id" {
-  count = length(var.sg_name)  
+resource "aws_ssm_parameter" "sg_id" { 
   name  = "/${var.project}/${var.environment}/mysql_root_password"    #/roboshop/dev/backend_alb-sg_id
   type  = "SecureString"
   value = var.mysql_root_password
