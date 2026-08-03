585fd3d31532df8300f3dc3df5bb4167e1267631
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-08-03 18:47:43 +0530

    api doc added

--- 40-database/r53.tf
+++ 40-database/r53.tf
@@ -5,6 +5,7 @@ resource "aws_route53_record" "mongodb" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
 }
 
 
@@ -14,6 +15,7 @@ resource "aws_route53_record" "redis" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
 }
 
 
@@ -23,6 +25,7 @@ resource "aws_route53_record" "rabbitmq" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "mysql" {
@@ -31,4 +34,5 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
 }
