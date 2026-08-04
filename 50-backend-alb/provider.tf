cc3990cda14a2e5b646c1384ff291b77522dae78
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-08-04 16:24:15 +0530

    api doc added

--- 50-backend-alb/provider.tf
+++ 50-backend-alb/provider.tf
@@ -8,7 +8,7 @@ terraform {
     # remote state storage location
   backend "s3" {
     bucket         = "demo-bucket-90s-dev" # create your own s3 bucket record 
-    key            = "roboshop-bastion.tfstate" 
+    key            = "roboshop-backend-alb.tfstate" 
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = true # Enables native S3 state locking (Terraform 1.10+)
