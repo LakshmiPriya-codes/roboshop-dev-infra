e591022e8ed831524e055d795e7e5936dbaca37f
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-08-13 17:22:17 +0530

    api doc added

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,9 +2,9 @@ locals {
      catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
      ami_id     =  data.aws_ami.joindevops.id
      vpc_id = data.aws_ssm_parameter.vpc_id.value
-     common_name = "${var.project}-${var.environment}" 
-      
+     common_name = "${var.project}-${var.environment}"   
      private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]  #roboshop-dev-private_subnet_ids
+     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
      common_tags = {
         Project = "${var.project}"
         Environment = "${var.environment}"
